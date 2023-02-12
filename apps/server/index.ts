bd6d4358e869389966e0e50f6226d2bc5a8d65d4
pisuttun <pisuttun@gmail.com>
2023-02-12 12:04:55 +0700

    remove unused code and fix type in server

--- apps/server/index.ts
+++ apps/server/index.ts
@@ -1,17 +1,17 @@
-import * as express from "express";
-import * as http from "http";
-import { Server } from "socket.io";
+import express from 'express'
+import * as http from 'http'
+import { Server } from 'socket.io'
 
-const app = express.default();
-const server = http.createServer(app);
-const io = new Server(server);
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server)
 
-io.on("connection", (socket) => {
-    socket.emit("your id", socket.id);
-    socket.on("send message", (body) => {
-        console.log(body);
-        io.emit("message", body);
-    });
-});
-const port = 8000;
-server.listen(port, () => console.log(`Listening on port:${port}...`));
+io.on('connection', (socket) => {
+  socket.emit('your id', socket.id)
+  socket.on('send message', (body) => {
+    console.log('receive: ', body)
+    io.emit('message', body)
+  })
+})
+const port = 8000
+server.listen(port, () => console.log(`Listening on port:${port}...`))
--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,31 +1,31 @@
-import React, { useState } from "react";
-import { io } from "socket.io-client";
+import React, { useState } from 'react'
+import { io } from 'socket.io-client'
 
-const socket = io("http://localhost:8000", { transports: ["websocket"] });
+const socket = io('http://localhost:8000', { transports: ['websocket'] })
 
 export default function Home() {
-    const [text, setText] = useState("");
-    const [id, setId] = useState("");
-    const [message, setMessage] = useState<string[]>([]);
-    const handlepost = () => {
-        socket.emit("send message", { post: text });
-    };
+  const [text, setText] = useState('')
+  const [id, setId] = useState('')
+  const [message, setMessage] = useState<string[]>([])
+  const handlepost = () => {
+    socket.emit('send message', { post: text })
+  }
 
-    socket.on("your id", (data) => {
-        setId(data);
-    });
-    socket.on("message", (data) => {
-        setMessage([...message, data.post]);
-    });
-    return (
-        <div>
-            <input type="text" onChange={(e) => setText(e.target.value)} />
-            <button onClick={handlepost}>Send massage </button>
-            <p>id: {id}</p>
-            <p>message:</p>
-            {message.map((p, index) => (
-                <li key={index}>{p}</li>
-            ))}
-        </div>
-    );
+  socket.on('your id', (data) => {
+    setId(data)
+  })
+  socket.on('message', (data) => {
+    setMessage([...message, data.post])
+  })
+  return (
+    <div>
+      <input type="text" onChange={(e) => setText(e.target.value)} />
+      <button onClick={handlepost}>Send massage </button>
+      <p>id: {id}</p>
+      <p>message:</p>
+      {message.map((p, index) => (
+        <li key={index}>{p}</li>
+      ))}
+    </div>
+  )
 }
