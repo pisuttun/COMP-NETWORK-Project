95f9b7c06b9fe094b81fefe3d6b559951807e4a1
Naphat <priasdevo@gmail.com>
2023-04-08 21:47:43 +0700

    feat: add global background

--- apps/web/src/modules/chat/components/MainFrame/components/ChatFrame/styled.ts
+++ apps/web/src/modules/chat/components/MainFrame/components/ChatFrame/styled.ts
@@ -3,7 +3,7 @@ import { styled } from '@mui/material'
 export const RootContainer = styled('div')`
   display: flex;
   flex-direction: row;
-  background-color: #412846;
+  background-color: transparent;
   width: 100%;
   border-bottom: 1px solid black;
   border-left: 1px solid black;
