c87e21cd9521ea33ed294e72dbdc2464a3b3edab
Naphat <priasdevo@gmail.com>
2023-04-09 23:29:32 +0700

    feat: draft Sidebar

--- apps/web/src/modules/chat/components/MainFrame/components/Sidebar/index.tsx
+++ apps/web/src/modules/chat/components/MainFrame/components/Sidebar/index.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import Typography from '@mui/material/Typography'
-import { RootContainer } from './styled'
+import { RootContainer, UserContainer, ChatChoiceContainer, IconContainer } from './styled'
+import { LogoutOutlined, KeyboardArrowUpOutlined } from '@mui/icons-material'
 
 export default function Sidebar() {
   return (
     <RootContainer>
-      <Typography sx={{ color: 'white' }}>Group</Typography>
+      <ChatChoiceContainer>
+        <Typography sx={{ color: 'white' }}>User1</Typography>
+        <Typography sx={{ color: 'white' }}>User2</Typography>
+      </ChatChoiceContainer>
+      <UserContainer>
+        <Typography sx={{ color: 'white' }}> Test</Typography>
+        <IconContainer>
+          <LogoutOutlined sx={{ fill: 'white', cursor: 'pointer' }} />
+          <KeyboardArrowUpOutlined sx={{ fill: 'white', cursor: 'pointer' }} />
+        </IconContainer>
+      </UserContainer>
     </RootContainer>
   )
 }
--- apps/web/src/modules/chat/components/MainFrame/components/Sidebar/styled.ts
+++ apps/web/src/modules/chat/components/MainFrame/components/Sidebar/styled.ts
@@ -3,7 +3,29 @@ import { styled } from '@mui/material'
 export const RootContainer = styled('div')`
   display: flex;
   flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
   background-color: #322036;
   width: 16vw;
   border-bottom: 1px solid black;
+`
+export const ChatChoiceContainer = styled('div')`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+`
+export const UserContainer = styled('div')`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 100%;
+  background-color: #1d121f;
+`
+
+export const IconContainer = styled('div')`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 10px;
 `
