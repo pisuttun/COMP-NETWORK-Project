3c9418ac1a6771129ac3d264932daf4129cf7f14
Naphat <priasdevo@gmail.com>
2023-04-10 22:25:13 +0700

    feat: draft chat name display in all location

--- apps/web/src/modules/chat/components/MainFrame/components/Sidebar/index.tsx
+++ apps/web/src/modules/chat/components/MainFrame/components/Sidebar/index.tsx
@@ -2,16 +2,17 @@ import React from 'react'
 import Typography from '@mui/material/Typography'
 import { RootContainer, UserContainer, ChatChoiceContainer, IconContainer } from './styled'
 import { LogoutOutlined, KeyboardArrowUpOutlined } from '@mui/icons-material'
+import ChatNameDisplay from 'modules/chat/components/ChatNameDisplay'
 
 export default function Sidebar() {
   return (
     <RootContainer>
       <ChatChoiceContainer>
-        <Typography sx={{ color: 'white' }}>User1</Typography>
-        <Typography sx={{ color: 'white' }}>User2</Typography>
+        <ChatNameDisplay isGroup={false} isOnline={true} name="Ice" />
+        <ChatNameDisplay isGroup={false} isOnline={false} name="Fain" />
       </ChatChoiceContainer>
       <UserContainer>
-        <Typography sx={{ color: 'white' }}> Test</Typography>
+        <ChatNameDisplay isGroup={false} isOnline={true} name="P" />
         <IconContainer>
           <LogoutOutlined sx={{ fill: 'white', cursor: 'pointer' }} />
           <KeyboardArrowUpOutlined sx={{ fill: 'white', cursor: 'pointer' }} />
--- apps/web/src/modules/chat/components/MainFrame/components/Sidebar/styled.ts
+++ apps/web/src/modules/chat/components/MainFrame/components/Sidebar/styled.ts
@@ -13,7 +13,10 @@ export const ChatChoiceContainer = styled('div')`
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
-  align-items: center;
+  align-items: flex-start;
+  width: 100%;
+  padding: 10px;
+  gap: 1rem;
 `
 export const UserContainer = styled('div')`
   display: flex;
@@ -22,6 +25,7 @@ export const UserContainer = styled('div')`
   align-items: center;
   width: 100%;
   height: 7vh;
+  padding: 18px;
   background-color: #1d121f;
 `
 
