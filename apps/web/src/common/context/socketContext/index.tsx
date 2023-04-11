d36e5a3211f01efae6a92d73b5b145fe52fd5da9
pisuttun <pisuttun@gmail.com>
2023-04-11 15:16:51 +0700

    feat: add page guard

--- apps/web/src/common/context/socketContext/index.tsx
+++ apps/web/src/common/context/socketContext/index.tsx
@@ -1,6 +1,8 @@
-import React, { createContext } from 'react'
+import React, { createContext, useEffect } from 'react'
 import { io } from 'socket.io-client'
 import { ISocketContext } from './types'
+import { useRouter } from 'next/router'
+import { useState } from 'react'
 
 const SocketContext = createContext<ISocketContext>({} as ISocketContext)
 
@@ -9,16 +11,23 @@ export const useSocket = () => React.useContext(SocketContext)
 export const SocketProvider = ({ children }: React.PropsWithChildren<{}>) => {
   const URL = process.env.NEXT_PUBLIC_DROPLET_URL || 'http://localhost:8000'
   const socket = io(URL, { transports: ['websocket'], reconnection: false })
+  const router = useRouter()
+
+  const [loading, setLoading] = useState(false)
 
   socket.on('connect', () => {
     if (typeof window !== 'undefined') {
+      //setLoading(true)
       socket.emit('verify token', {
         token: localStorage.getItem('token') || '',
       })
     }
     socket.on('verify status', (data: { isSuccess: boolean }) => {
       console.log(data)
-      //TODO: redirect to login page if isSuccess is false
+      if (data.isSuccess === false && router.pathname !== '/') {
+        router.replace('/')
+      }
+      setLoading(false)
     })
   })
 
