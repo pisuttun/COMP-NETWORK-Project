0f87904e39ef5ddcd6bb0bdac4c2c456178130d7
SuitedIceZ <okshopm575@gmail.com>
2023-04-29 12:26:30 +0700

    feat: protect rest route

--- apps/server/routes/messages.ts
+++ apps/server/routes/messages.ts
@@ -1,12 +1,8 @@
 import express from 'express'
 import { handleGetAllMessage } from '../controllers/messages'
+import { protectRESTRoute } from '../middleware/auth'
 const router = express.Router()
 
-router.route('/').get((req, res) => {
-  console.log('get all chat data')
-  //   res.status(200).json({ message: 'get all chat data' })
-  console.log('in router req params query: ', req.query)
-  handleGetAllMessage(req, res)
-})
+router.route('/').get(protectRESTRoute, handleGetAllMessage)
 
 export default router
